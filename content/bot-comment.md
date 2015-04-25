5b33d0a6ce3632f81ba0be2521aeefa3e3be1614
Uwe Arzt <mail@uwe-arzt.de>
2015-04-25 11:38:39 +0200

    Added recaptcha to comments

---
+++ content/bot-comment.md
@@ -0,0 +1,9 @@
++++
+title = "Comment from Bot"
+type = "page"
+categories = [""]
++++
+
+ReCaptcha thinks you are a bot. If you think that is wrong, please EMail your comment
+to the site Administrator.
+
--- publish.sh
+++ publish.sh
@@ -4,3 +4,5 @@ hugo --verbose
 rsync -r -c --delete --progress public/* uwe-arzt.de:site-uwe-arzt
 rsync etc/htaccess uwe-arzt.de:site-uwe-arzt/.htaccess
 rsync etc/robots.txt uwe-arzt.de:site-uwe-arzt/robots.txt
+rsync ${HOME}/.recaptcha/private-key uwe-arzt.de:site-uwe-arzt/cgi-bin
+
