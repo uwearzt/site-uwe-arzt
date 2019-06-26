8d7875500a5355842d4a52ef42a9bba189c8bd5b
Uwe Arzt <mail@uwe-arzt.de>
2019-06-26 22:39:51 +0200

    upload only to hetzner

--- publish.sh
+++ publish.sh
@@ -16,37 +16,13 @@ cobalt --version
 echo "---------------------------------------------------------------------"
 
 # ------------------------------------------------------------------------------
-SERVER=uwe-arzt.de
-DIR=site-uwe-arzt
-
 NEWSERVER=hetzner01
-NEWDIR=/var/www/site-uwe-arzt-de
+NEWDIR=/var/www/site-uwe-arzt
 
 # ------------------------------------------------------------------------------
 cobalt clean
 # cobalt --log-level=trace build
 cobalt build
-
-# ------------------------------------------------------------------------------
-echo "-------------------------------------------------------------------------"
-echo "changed files on 1und1"
-rsync --dry-run --filter='P **.log' -r -c --delete --progress build/* ${SERVER}:${DIR}/
-
-echo "-------------------------------------------------------------------------"
-echo "upload?"
-select option in Yes No
-do
-    case $option in
-        Yes) 
-            rsync -r -c --filter='P **.log' --delete --progress  build/* ${SERVER}:${DIR}/
-            rsync etc/htaccess ${SERVER}:${DIR}/.htaccess
-            rsync etc/robots.txt ${SERVER}:${DIR}/robots.txt
-            break;;
-        No)
-            echo "Not uploading"
-            break;;
-     esac
-done
 
 # ------------------------------------------------------------------------------
 echo "-------------------------------------------------------------------------"
